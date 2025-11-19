293febe1dc179ccd506e5ef00779faac63712457
shamil mt <shamilsham248@gmail.com>
2025-11-19 19:03:40 +0400

    now setted

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-  fetch("/nav.html")
+  fetch("./nav.html")
     .then((response) => response.text())
     .then((data) => {
       document.getElementById("navbar-container").innerHTML = data;
